85628f62b7287c2b82e3e835f94f7353b08b1f8b
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-09-21 23:34:40 +0300

    Edit Main

--- src/main/java/ua/alexkras/Main.java
+++ src/main/java/ua/alexkras/Main.java
@@ -8,7 +8,39 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<Integer> arrayList = new ArrayList<>(
+                Arrays.asList(4,5,-6,4,5,3,4,2,4,5,7));
 
+        System.out.println(arrayList);
+
+        Counter counter = new Counter(arrayList);
+
+        counter.printCounter();
+
+        NoDeletionArrayList<Integer> list = new NoDeletionArrayList<>();
+
+        list.add(1);
+        list.addAll(Arrays.asList(2,3,4,5,6));
+        list.add(-1);
+
+        System.out.println(list);
+
+        NoDeletionLinkedList<Integer> integers = new NoDeletionLinkedList<>(
+                Arrays.asList(40,2,3,4,5,6));
+
+
+        integers.addFirst(30);
+
+        System.out.printf("size: %d first: %d last: %d\n",integers.size(),integers.getFirst(),integers.getLast());
+
+
+        System.out.println(integers);
+
+        integers.addLast(50);
+        System.out.println(integers);
+
+        System.out.printf("integers[%d] = %d\n",2,integers.get(2));
+        integers.get(8); //IndexOutOfBoundsException
 
     }
 }
