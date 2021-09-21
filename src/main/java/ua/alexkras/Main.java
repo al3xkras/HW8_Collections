b6774e9e33731418b645347eb9bc70261f37c3da
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-09-21 23:29:58 +0300

    Add Main.java

---
+++ src/main/java/ua/alexkras/Main.java
@@ -0,0 +1,14 @@
+package ua.alexkras;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
